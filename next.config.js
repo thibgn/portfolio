bd2481de49bde7fc4d96f1fb1d3da2758e0afd8b
Thibaud Gerin <thibaud.gerin@gmail.com>
2021-11-22 11:03:12 +0100

    img host fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
 	reactStrictMode: true,
 	images: {
 		domains: [
@@ -9,19 +8,7 @@ module.exports = {
 			'www.notion.so',
 			'images.unsplash.com',
 			'www.memesticks.club',
+			'memesticks.club',
 		],
 	},
-=======
-  reactStrictMode: true,
-  images: {
-    domains: [
-      'www.notion.com',
-      'via.placeholder.com',
-      's3.us-west-2.amazonaws.com',
-      'www.notion.so',
-      'images.unsplash.com',
-      'www.memesticks.club',
-    ],
-  },
->>>>>>> d16422104419c666c5b49a8d89cf1ae4dc36b499
 };
